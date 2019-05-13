2594b879eec71b853fc248ee028962313a1569d9
Marc Köhlbrugge <subscriptions@marckohlbrugge.com>
2019-05-13 16:08:10 +0900

    add http methods

--- Gemfile
+++ Gemfile
@@ -1,2 +1,4 @@
 source "http://rubygems.org"
 gem "sinatra"
+
+gem "http", "~> 4.1"
--- app.rb
+++ app.rb
@@ -1,5 +1,34 @@
 require "sinatra"
+require "http"
 
 get "/" do
   "Hello World!"
+end
+
+get "/get" do
+  HTTP.get(url).to_s
+end
+
+get "/post" do
+  HTTP.post(url).to_s
+end
+
+get "/put" do
+  HTTP.put(url).to_s
+end
+
+get "/patch" do
+  HTTP.patch(url).to_s
+end
+
+get "/delete" do
+  HTTP.delete(url).to_s
+end
+
+get "/head" do
+  HTTP.head(url).to_s
+end
+
+def url
+  params[:url]
 end
