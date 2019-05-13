f4fff5c003bcb3e970e04fee01c82756e68b6509
Marc Köhlbrugge <subscriptions@marckohlbrugge.com>
2019-05-13 16:11:32 +0900

    follow redirects

--- app.rb
+++ app.rb
@@ -6,27 +6,27 @@ get "/" do
 end
 
 get "/get" do
-  HTTP.get(url).to_s
+  HTTP.follow.get(url).to_s
 end
 
 get "/post" do
-  HTTP.post(url).to_s
+  HTTP.follow.post(url).to_s
 end
 
 get "/put" do
-  HTTP.put(url).to_s
+  HTTP.follow.put(url).to_s
 end
 
 get "/patch" do
-  HTTP.patch(url).to_s
+  HTTP.follow.patch(url).to_s
 end
 
 get "/delete" do
-  HTTP.delete(url).to_s
+  HTTP.follow.delete(url).to_s
 end
 
 get "/head" do
-  HTTP.head(url).to_s
+  HTTP.follow.head(url).to_s
 end
 
 def url
